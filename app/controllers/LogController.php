5da1dec3bd3e42e97d247604b8640f0c8374269a
Kehinde <kolexinfos@gmail.com>
2014-03-27 14:22:01 +0100

    LogController Completed

--- app/controllers/LogController.php
+++ app/controllers/LogController.php
@@ -3,14 +3,136 @@
 class LogController extends BaseController {
 
     public  function QuestionCompleted(){
+        $input=Input::all();
 
+        $rules=array(
+            'Exam'=>'required',
+            'Course'=>'required',
+            'Userid'=>'required',
+            'QuestionId'=>'required',
+            'Year'=>'required',
+            'Status'=>'required',
+            'Number'=>'required',
+            'Answer'=>'required',
+        );
 
+        $v=Validator::make($input,$rules);
+        if($v->fails())
+        {
+            return Response::json(
+                array(
+                    'error'=>'Incomplete Values Sent',
+                    'message'=>$v->messages()->first('Exam')
+                ),400);
+        }
 
+        $newLog = new Userlog();
+        $newLog->UserActionsId = 4;
+        $newLog->UserId = Input::get('Userid');
+        $newLog->UserLogDetails = "QuestionCompleted";
+        $newLog->save();
+
+        $LogId = $newLog->UserLogId;
+
+        foreach ($input as $k => $v) {
+
+            $newField = new Userlogfield();
+            $newField->userlogid = $LogId;
+            $newField->name = $k;
+            $newField->value = $v;
+            $newField->save();
+
+        }
+
+        return Response::json(array('flash' => 'Question Completed Log Created'), 200);
 
     }
 
     public  function TestCompleted(){
+        $input=Input::all();
 
+        $rules=array(
+            'Exam'=>'required',
+            'Course'=>'required',
+            'Userid'=>'required',
+            'Year'=>'required',
+            'Status'=>'required',
+            'Result'=>'required',
+            'Duration'=>'required'
+        );
+
+        $v=Validator::make($input,$rules);
+        if($v->fails())
+        {
+            return Response::json(
+                array(
+                    'error'=>'Incomplete Values Sent',
+                    'message'=>$v->messages()->first('Exam')
+                ),400);
+        }
+
+        $newLog = new Userlog();
+        $newLog->UserActionsId = 3;
+        $newLog->UserId = Input::get('Userid');
+        $newLog->UserLogDetails = "TestCompleted";
+        $newLog->save();
+
+        $LogId = $newLog->UserLogId;
+
+        foreach ($input as $k => $v) {
+
+            $newField = new Userlogfield();
+            $newField->userlogid = $LogId;
+            $newField->name = $k;
+            $newField->value = $v;
+            $newField->save();
+
+        }
+        return Response::json(array('flash' => 'Test Completed Log Created'), 200);
+    }
+
+    public function ViewedNote(){
+        $input=Input::all();
+
+        $rules=array(
+            'Exam'=>'required',
+            'Course'=>'required',
+            'Userid'=>'required',
+            'QuestionId'=>'required',
+            'Year'=>'required',
+            'Status'=>'required',
+            'Number'=>'required',
+            'Answer'=>'required',
+        );
+
+        $v=Validator::make($input,$rules);
+        if($v->fails())
+        {
+            return Response::json(
+                array(
+                    'error'=>'Incomplete Values Sent',
+                    'message'=>$v->messages()->first('Exam')
+                ),400);
+        }
+
+        $newLog = new Userlog();
+        $newLog->UserActionsId = 5;
+        $newLog->UserId = Input::get('Userid');
+        $newLog->UserLogDetails = "ViewedNote";
+        $newLog->save();
+
+        $LogId = $newLog->UserLogId;
+
+        foreach ($input as $k => $v) {
+
+            $newField = new Userlogfield();
+            $newField->userlogid = $LogId;
+            $newField->name = $k;
+            $newField->value = $v;
+            $newField->save();
+
+        }
+        return Response::json(array('flash' => 'Note Viewed Log Created'), 200);
     }
 
 
--- app/controllers/StatController.php
+++ app/controllers/StatController.php
@@ -3,10 +3,26 @@
 class LogController extends BaseController {
 
   public function getCompletedQuestions() {
+      $input=Input::all();
+
+      $rules=array(
+
+          'Userid'=>'required'
+      );
+
+      $v=Validator::make($input,$rules);
+      if($v->fails())
+      {
+          return Response::json(
+              array(
+                  'error'=>'Incomplete Values Sent',
+                  'message'=>$v->messages()->first('Userid')
+              ),400);
+      }
 
 
 
-     }
+           }
 
     public function getCompletedTests() {
 
