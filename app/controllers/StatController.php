5221f5dadddc3af644fe90dfa1c9a06716ec60de
Kehinde <kolexinfos@gmail.com>
2014-04-11 13:23:05 +0100

    Updated StatController

--- app/controllers/StatController.php
+++ app/controllers/StatController.php
@@ -1,6 +1,6 @@
 <?php
 
-class LogController extends BaseController {
+class StatController extends BaseController {
 
   public function getCompletedQuestions() {
       $input=Input::all();
@@ -10,6 +10,7 @@ class LogController extends BaseController {
       );
 
       $v=Validator::make($input,$rules);
+
       if($v->fails())
       {
           return Response::json(
@@ -19,14 +20,58 @@ class LogController extends BaseController {
               ),400);
       }
 
- if
+       $userid = Input::get('Userid');
+
+      $logs =DB::select( DB::raw("select count(*) as count from userlogs WHERE userlogdetails = 'QuestionCompleted' and userid = :userid"), array(
+          'userid'=> $userid));
+
+       return Response::json($logs,200);
 
      }
 
     public function getCompletedTests() {
+        $input=Input::all();
+
+        $rules=array(
+            'Userid'=>'required'
+        );
+
+        $v=Validator::make($input,$rules);
+        if($v->fails())
+        {
+            return Response::json(
+                array(
+                    'error'=>'Incomplete Values Sent',
+                    'message'=>$v->messages()->first('Userid')
+                ),400);
+        }
+
+        $userid = Input::get('Userid');
+
+        $logs =DB::select( DB::raw("select count(*) as count from userlogs WHERE userlogdetails = 'TestCompleted' and userid = :userid"), array(
+            'userid'=> $userid));
+
+        return Response::json($logs,200);
 
     }
 
-    public function getSuccess(){ }
+    public function getSuccess(){
+        $input=Input::all();
+
+        $rules=array(
+            'Userid'=>'required'
+        );
+
+        $v=Validator::make($input,$rules);
+        if($v->fails())
+        {
+            return Response::json(
+                array(
+                    'error'=>'Incomplete Values Sent',
+                    'message'=>$v->messages()->first('Userid')
+                ),400);
+        }
+
+    }
 
 }
